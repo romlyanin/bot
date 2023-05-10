5c5ad8136f99c798a2af08ed0798fd93c935db33
romlyanin <131984193+romlyanin@users.noreply.github.com>
2023-05-10 13:13:20 +0200

    Update send_message.php

--- send_message.php
+++ send_message.php
@@ -1,33 +1,34 @@
 
 <?php
-
-$botToken = '6020094789:AAEraLAOChuB8Y5qp8uNk1Q4GsITY8N7TYE';
-$chatID = '330685';
+// Получение параметра сообщения
 $message = $_POST['message'];
 
-$apiUrl = "https://api.telegram.org/bot{$botToken}/sendMessage";
+// Замените 'YOUR_BOT_TOKEN' на свой токен бота Telegram
+$botToken = '6020094789:AAEraLAOChuB8Y5qp8uNk1Q4GsITY8N7TYE';
+// Замените 'TARGET_USER_ID' на фактический идентификатор пользователя
+$chatID = '330685';
+
+// Формирование URL для отправки запроса
+$url = "https://api.telegram.org/bot" . $botToken . "/sendMessage";
 $data = array(
     'chat_id' => $chatID,
     'text' => $message
 );
 
-$ch = curl_init($apiUrl);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+$options = array(
+    'http' => array(
+        'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+        'method' => 'POST',
+        'content' => http_build_query($data)
+    )
+);
+$context = stream_context_create($options);
+$result = file_get_contents($url, false, $context);
 
-$response = curl_exec($ch);
-
-if ($response === false) {
-    // Обработка ошибки
-    $error = curl_error($ch);
-    echo "Ошибка при выполнении запроса: {$error}";
+if ($result === false) {
+    echo "Ошибка при отправке сообщения в Telegram.";
 } else {
-    // Обработка успешного результата
     echo "Сообщение успешно отправлено в Telegram!";
 }
-
-curl_close($ch);
-
 ?>
 
